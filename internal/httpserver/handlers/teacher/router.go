3570de10caa44b1682ef9af22587669c8387417a
khostya <khostya.konstantin@gmail.com>
2023-09-14 19:33:15 +0500

    запретил всем не авторам создавать курсы

--- internal/httpserver/handlers/teacher/router.go
+++ internal/httpserver/handlers/teacher/router.go
@@ -11,14 +11,15 @@ import (
 
 func New(r *chi.Mux, useCases usecase.UseCases) {
 	useCase := useCases.Teacher
+	tokenManager := useCases.Deps.TokenManager
 
 	r.Route("/v1/teacher", func(r chi.Router) {
 		r.Group(func(r chi.Router) {
-			r.Use(mwAuth.Required(useCases.Deps.TokenManager))
+			r.Use(mwAuth.Required(tokenManager))
 			r.Post("/", create.New(useCase))
 
 			r.Group(func(r chi.Router) {
-				r.Use(role.Teacher(useCases.Deps.TokenManager))
+				r.Use(role.Teacher(tokenManager))
 				r.Get("/courses", courses.New(useCase))
 			})
 		})
---
+++ internal/httpserver/middleware/role/author.go
@@ -0,0 +1,30 @@
+package role
+
+import (
+	"github.com/radium-rtf/radium-backend/pkg/auth"
+	"net/http"
+	"strings"
+)
+
+func Author(manager auth.TokenManager) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			tokenHeader := strings.Split(request.Header.Get("Authorization"), " ")
+
+			isAuthor, err := manager.ExtractIsAuthor(tokenHeader)
+			if err != nil {
+				writer.WriteHeader(http.StatusUnauthorized)
+				writer.Write([]byte(err.Error()))
+				return
+			}
+
+			if !isAuthor {
+				writer.WriteHeader(http.StatusForbidden)
+				writer.Write([]byte("нет роли автора, попробуй перезайти (роль вшита в токен)"))
+				return
+			}
+
+			next.ServeHTTP(writer, request)
+		})
+	}
+}
