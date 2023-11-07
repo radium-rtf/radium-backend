aa0762ded2df8c506b30b964dbe1c9db244b3d24
khostya <khostya.konstantin@gmail.com>
2023-11-08 01:21:04 +0500

    fix: not null is_published

--- migrations/20231102105536_add_is_published_course.up.sql
+++ migrations/20231102105536_add_is_published_course.up.sql
@@ -2,7 +2,7 @@ SET statement_timeout = 0;
 
 --bun:split
 
-alter table courses add column is_published bool default false;
+alter table courses add column is_published bool not null default false;
 
 --bun:split
 
