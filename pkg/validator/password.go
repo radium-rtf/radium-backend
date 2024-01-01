71963b6bfea88b41b54fb0a4e620be7700743298
khostya <khostya.konstantin@gmail.com>
2024-01-01 19:46:03 +0500

    fix: валидация пароля

--- pkg/validator/password.go
+++ pkg/validator/password.go
@@ -10,16 +10,15 @@ import (
 const (
 	passwordMinLength = 8
 	passwordMaxLength = 32
-	passwordMinLower  = 1
-	passwordMinUpper  = 1
+	passwordMinChar   = 1
 	passwordMinDigit  = 1
 	passwordMinSymbol = 1
 )
 
 var (
 	lengthRegexp    = regexp.MustCompile(fmt.Sprintf(`^.{%d,%d}$`, passwordMinLength, passwordMaxLength))
-	lowerCaseRegexp = regexp.MustCompile(fmt.Sprintf(`[a-z]{%d,}`, passwordMinLower))
-	upperCaseRegexp = regexp.MustCompile(fmt.Sprintf(`[A-Z]{%d,}`, passwordMinUpper))
+	lowerCaseRegexp = regexp.MustCompile(fmt.Sprintf(`[a-z]{%d,}`, passwordMinChar))
+	upperCaseRegexp = regexp.MustCompile(fmt.Sprintf(`[A-Z]{%d,}`, passwordMinChar))
 	digitRegexp     = regexp.MustCompile(fmt.Sprintf(`[0-9]{%d,}`, passwordMinDigit))
 	symbolRegexp    = regexp.MustCompile(fmt.Sprintf(`[^A-Za-z0-9]{%d,}`, passwordMinSymbol))
 )
@@ -36,20 +35,16 @@ func (v *validate) passwordValidate(fl validator.FieldLevel) bool {
 		v.passwdErr = fmt.Errorf("field %s must be between %d and %d characters", fl.FieldName(), passwordMinLength, passwordMaxLength)
 		return false
 	}
-	if ok := lowerCaseRegexp.MatchString(fieldValue); !ok {
-		v.passwdErr = fmt.Errorf("field %s must contain at least %d lowercase letter(s)", fl.FieldName(), passwordMinLower)
+
+	lowerCaseMatch := lowerCaseRegexp.MatchString(fieldValue)
+	upperCaseMatch := upperCaseRegexp.MatchString(fieldValue)
+	if !lowerCaseMatch && !upperCaseMatch {
+		v.passwdErr = fmt.Errorf("field %s must contain at least %d (lower/upper)case letter(s)", fl.FieldName(), passwordMinChar)
 		return false
 	}
-	if ok := upperCaseRegexp.MatchString(fieldValue); !ok {
-		v.passwdErr = fmt.Errorf("field %s must contain at least %d uppercase letter(s)", fl.FieldName(), passwordMinUpper)
-		return false
-	}
+
 	if ok := digitRegexp.MatchString(fieldValue); !ok {
 		v.passwdErr = fmt.Errorf("field %s must contain at least %d digit(s)", fl.FieldName(), passwordMinDigit)
-		return false
-	}
-	if ok := symbolRegexp.MatchString(fieldValue); !ok {
-		v.passwdErr = fmt.Errorf("field %s must contain at least %d special character(s)", fl.FieldName(), passwordMinSymbol)
 		return false
 	}
 
