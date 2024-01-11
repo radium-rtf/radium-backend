620d55a6824649167b2f67956de765b73f657f54
khostya <khostya.konstantin@gmail.com>
2024-01-12 00:48:47 +0500

    fix

--- internal/httpserver/handlers/logger.go
+++ internal/httpserver/handlers/logger.go
@@ -1,8 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httplog/v2"
 	"log/slog"
 	"net/http"
@@ -21,36 +19,17 @@ func newLogger() *httplog.Logger {
 }
 
 func newHandlerLogger(log *httplog.Logger) func(h http.Handler) http.Handler {
-	paths := []string{"/auth/", "/password", "/swagger/"}
+	paths := []string{"/swagger/"}
 	logfn := httplog.Handler(log)
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			for _, path := range paths {
 				if strings.Contains(request.URL.Path, path) {
-					var f middleware.LogFormatter = &requestLogger{*log.Logger, log.Options}
-
-					entry := f.NewLogEntry(request)
-					next.ServeHTTP(writer, middleware.WithLogEntry(request, entry))
 					return
 				}
 			}
 			logfn(next).ServeHTTP(writer, request)
 		})
 	}
-}
-
-type requestLogger struct {
-	Logger  slog.Logger
-	Options httplog.Options
-}
-
-func (l *requestLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
-	entry := &httplog.RequestLoggerEntry{}
-	msg := fmt.Sprintf("Request: %s %s", r.Method, r.URL.Path)
-
-	if !l.Options.Concise {
-		entry.Logger.Info(msg)
-	}
-	return entry
 }
