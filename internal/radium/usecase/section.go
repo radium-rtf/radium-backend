fc37b36f16d07d58bc91841d2e171368ec020b9e
khostya <khostya.konstantin@gmail.com>
2024-03-31 22:03:56 +0500

    feat: get section answer

--- internal/radium/usecase/section.go
+++ internal/radium/usecase/section.go
@@ -75,10 +75,6 @@ func (uc SectionUseCase) UpdateOrder(ctx context.Context, id, editorId uuid.UUID
 }
 
 func (uc SectionUseCase) GetFullSectionById(ctx context.Context, id, editorId uuid.UUID) (*entity.Section, error) {
-	canEditErr := uc.canEdit(ctx, id, editorId)
-	if canEditErr != nil {
-		return nil, errors.New("нет прав на просмотр ответов секций")
-	}
 	return uc.section.GetById(ctx, id)
 }
 
