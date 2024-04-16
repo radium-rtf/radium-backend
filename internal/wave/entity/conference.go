4392949d4d1e98ac3451b125f5b5cb45b1ab0b7e
FUFSoB <fufsob@fufsob.ru>
2024-04-16 19:11:47 +0500

    expand on conference roles

--- internal/wave/entity/conference.go
+++ internal/wave/entity/conference.go
@@ -24,10 +24,13 @@ type (
 		DBModel
 
 		ConferenceId uuid.UUID `bun:",pk"`
+
+		Roles []*ConferenceRole `bun:"rel:has-many"`
 	}
 
 	ConferenceSubscribers struct {
 		bun.BaseModel `bun:"table:wave.conference_subscriber"`
+		DBModel
 
 		ConferenceId uuid.UUID   `bun:",pk"`
 		Conference   *Conference `bun:"rel:belongs-to,join:conference_id=id"`
@@ -35,16 +38,49 @@ type (
 		UserId uuid.UUID    `bun:",pk"`
 		User   *radium.User `bun:"rel:belongs-to,join:user_id=id"`
 
-		RoleCode string
-		Role     *ConferenceRole `bun:"rel:belongs-to,join:role_code=code"`
+		Roles []*ConferenceRole `bun:"m2m:wave.conference_subscriber_role,join:Subscriber=Role"`
+	}
+
+	ConferenceSubscriberRoles struct {
+		bun.BaseModel `bun:"table:wave.conference_subscriber_role"`
+
+		SubscriberId uuid.UUID              `bun:",pk"`
+		Subscriber   *ConferenceSubscribers `bun:"rel:belongs-to,join=subscriber_id=id"`
+
+		RoleId uuid.UUID       `bun:",pk"`
+		Role   *ConferenceRole `bun:"rel:belongs-to,join:role_id=id"`
 	}
 
 	ConferenceRole struct {
 		bun.BaseModel `bun:"table:wave.conference_role"`
 		DBModel
 
-		Code string `bun:",unique"`
-		Name string
+		Name string `bun:",notnull,unique"`
+
+		Permissions ConferenceRolePermissions `bun:"rel:has-one"`
+	}
+
+	ConferenceRolePermissions struct {
+		bun.BaseModel `bun:"table:wave.group_role_permissions"`
+
+		ConferenceRoleId uuid.UUID `bun:",pk"`
+
+		// Absolute
+		Administrator bool
+		// Moderation
+		DeleteMessages bool
+		AddUsers       bool
+		KickUsers      bool
+		MuteUsers      bool
+		// General
+		Write           bool
+		CreateThreads   bool
+		AttachFiles     bool
+		MentionEveryone bool
+		StartCalls      bool
+		Speak           bool
+		EnableCamera    bool
+		ScreenShare     bool
 	}
 
 	ConferenceCommentSection struct {
